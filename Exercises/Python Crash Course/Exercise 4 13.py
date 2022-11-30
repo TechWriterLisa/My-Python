77b584474c755686d1bd8bfc678ff72379864d19
TechWriterLisa <ldadams42@gmail.com>
2022-11-30 14:22:53 -0600

    New Work

---
+++ Exercises/Python Crash Course/Exercise 4 13.py
@@ -0,0 +1,13 @@
+myTup = ('eggs', 'beans', 'corn', 'natto', 'ham')
+
+for count in range(5):
+
+    print(myTup[count])
+
+#myTup[0]= 'bread'
+
+myTup = ('eggs', 'beans', 'corn', 'beets', 'turkey')
+
+for count in range(5):
+
+    print(myTup[count])
